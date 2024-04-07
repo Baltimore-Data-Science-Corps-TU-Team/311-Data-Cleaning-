1082576b74f056d73e0e70c8ac1afb029a8bcb1c
Simon Yoseph <syosep1@students.towson.edu>
2024-04-07 17:21:14 -0400

    created seperate files for programs
    created seperate files for programs

--- clean_311_Data.py
+++ clean_311_Data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import csv
 
-
 # Below are 3 functions with the description of each starting with #### 
 # and ending with a row of #########################
 
@@ -38,81 +37,8 @@ import csv
         #24 'GeoLocation': string
 
 
-#### Prints total lines for csv ####
-
-# Initialize count
-# count = 0  
-
-# with open('311_CSR_2021.csv') as file:
-#     csv_reader = csv.reader(file)
-#     for line in csv_reader:
-
-#         # Increment count for each iteration
-#         count += 1       
-
-#         # Print the 6th element and count  
-#         print(f"Line {count}: {line[5]}")   
-
-#         # Print the total count after the loop
-#         print(f"Total lines: {count}")  
-
-#########################
 
 
-#### List all SR types and save to new file####
 
-sr_types = []
-
-with open('311_CSR_2021.csv') as file:
-    csv_reader = csv.reader(file)
-    for line in csv_reader:
-
-        # Accessing the SRType column (index 5)
-        sr_type = line[5]  
-        sr_types.append(sr_type)
-        print(sr_type)
-
- # Print the total number of SR types
-total_sr_types = len(sr_types)
-print(f"Total number of SR types: {total_sr_types}")
-
-with open('311_CSR_2021.csv') as file:
-     csv_reader = csv.reader(file)
-     for line in csv_reader:
-         
-         # Accessing the SRType column (index 5)
-         sr_type = line[5]  
-         print(sr_type)
-
-# Save SR types to a new file
-with open('filtered_ srTypes.csv', 'w', newline='') as output_file:
-    csv_writer = csv.writer(output_file)
-    csv_writer.writerow(['SR Type'])
-    for sr_type in sr_types:
-        csv_writer.writerow([sr_type])
-
-print("SR types saved to filtered_ srTypes.csv")
-
-
-#########################
-
-
-#### Total number of unique SRTypes ####
-
-# Set to store unique SR types
-# sr_types = set()
-
-# with open('311_CSR_2021.csv') as file:
-#     csv_reader = csv.reader(file)
-#     for line in csv_reader:
-#         sr_type = line[5]  # Accessing the SRType column (index 5)
-#         sr_types.add(sr_type)
-
-# # Print the total number of unique SR types
-# total_sr_types = len(sr_types)
-# print(sr_type)
-# print(f"Total number of SR types: {total_sr_types}")
-
-#########################    
 
    
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,14 @@
-sr_types = []
+count = 0  
 
 with open('311_CSR_2021.csv') as file:
     csv_reader = csv.reader(file)
     for line in csv_reader:
-        sr_type = line[5]  # Accessing the SRType column (index 5)
-        sr_types.append(sr_type)
-        print(sr_type)
+
+        # Increment count for each iteration
+        count += 1       
+
+        # Print the 6th element and count  
+        print(f"Line {count}: {line[5]}")   
+
+        # Print the total count after the loop
+        print(f"Total lines: {count}")  
